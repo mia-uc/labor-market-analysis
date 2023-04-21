926bef4300f41cb3775b2af0646d2383fb7a496e
danielorlando97 <danielorlandoortizpacheco@gmail.com>
2023-04-21 11:19:38 -0400

    fix bugs in getonboard

--- src/scrapers/getonboard/find_details.py
+++ src/scrapers/getonboard/find_details.py
@@ -28,14 +28,14 @@ def get_details_for_job(job: dict, db: MongoInterfaces):
     body = soup.find("div", {"id": "job-body", "itemprop": 'description'})
     new_info['body'] = body.get_text().strip()
 
-    apply_bottom = soup.find("a", {"id": "apply_bottom"})
-    new_info["url_to_apply"] = apply_bottom.get('href')
+    if (apply_bottom := soup.find("a", {"id": "apply_bottom"})):
+        new_info["url_to_apply"] = apply_bottom.get('href')
 
-    quick_apply_bottom = soup.find("a", {"id": "quick_apply_bottom"})
-    new_info["url_to_apply_quick"] = None if quick_apply_bottom is None else quick_apply_bottom.get('href')
+    if (quick_apply_bottom := soup.find("a", {"id": "quick_apply_bottom"})):
+        new_info["url_to_apply_quick"] = quick_apply_bottom.get('href')
     
-    tags = soup.find('div', {"class":"gb-tags", "itemprop":"skills"})
-    if tags:
+    
+    if (tags := soup.find('div', {"class":"gb-tags", "itemprop":"skills"})):
         new_info["jobs_tags"] = [a.get_text().strip() for a in tags.find_all('a')]
 
     header = soup.find('div', {'class': 'gb-company-theme-colored'})
